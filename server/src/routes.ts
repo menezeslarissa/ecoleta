e4d1d5415f9b473ad436e0b6542111f819664559
Larissa Menezes <menezes.larissaps@gmail.com>
2020-06-05 15:47:40 -0300

    criando método para salvar ponto de coleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,56 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
+// listagem de items
+routes.get('/items', async (request, response) => {
+   const items = await knex('itens').select('*');
 
-routes.get('/', (request, response) => {
-    return response.json([
-        'Larissa'
-    ]);
+   const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title : item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        };
+   });
+   return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    // recurso de desestruturação do javascript
+    // coloca cada campo em um variavel diferente qnd sabe qual formato do body
+    const {
+        name,
+        email,
+        whatsapp,
+        lat,
+        lon,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    // short syntax
+    const ids = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        lat,
+        lon,
+        city,
+        uf
+    });
+
+    const pointItems = items.map((item_id: number) => {
+        return {
+            item_id,
+            point_id: ids[0],
+        };
+    });
+    await knex('point_itens').insert(pointItems);
+    return response.json({ success: true})
 });
 
 export default routes;
